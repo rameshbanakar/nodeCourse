b905ea3e24775d57a559b88bb074a9dcd2043854
Ramesh S Banakar <109899161+rameshbanakar@users.noreply.github.com>
2022-12-24 20:15:28 +0530

    Delete Desktop/Training files/birthdayNotification directory

--- Desktop/Training files/birthdayNotification/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app
-  .get("/", (req, res) => {
-    res.send("hello");
-  })
-  .listen(5000, () => console.log("server started@localhost 5000"));
